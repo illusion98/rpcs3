bf50c20d6e8be116a405f675aa63c9e1e07027e5
Rajko Stojadinovic <admin@rajko.info>
2019-10-02 20:15:48 +0200

    use "ZCull query default value" video setting as default output value when ZCull occulsion queries are disabled

--- rpcs3/Emu/system_config.h
+++ rpcs3/Emu/system_config.h
@@ -181,6 +181,7 @@ struct cfg_root : cfg::node
 		cfg::_bool full_rgb_range_output{ this, "Use full RGB output range", true, true }; // Video out dynamic range
 		cfg::_bool strict_texture_flushing{ this, "Strict Texture Flushing", false };
 		cfg::_bool multithreaded_rsx{ this, "Multithreaded RSX", false };
+		cfg::_int<0, 0xFFFFFFFFu> zcull_default_value{this, "ZCull query default value", 1};
 		cfg::_bool relaxed_zcull_sync{ this, "Relaxed ZCULL Sync", false };
 		cfg::_enum<stereo_render_mode_options> stereo_render_mode{ this, "3D Display Mode", stereo_render_mode_options::disabled };
 		cfg::_bool debug_program_analyser{ this, "Debug Program Analyser", false };
