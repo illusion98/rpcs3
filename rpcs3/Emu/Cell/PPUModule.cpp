275494dcf2ea2f61c8a548368762a7a8daab81e7
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2022-08-30 12:48:04 -0500

    Comment `Already linked function` log spam

--- rpcs3/Emu/Cell/PPUModule.cpp
+++ rpcs3/Emu/Cell/PPUModule.cpp
@@ -807,10 +807,12 @@ static auto ppu_load_exports(const ppu_module& _module, ppu_linkage_info* link, 
 				flink.export_addr = 0;
 			}
 
+			/*
 			if (flink.export_addr)
 			{
 				ppu_loader.notice("Already linked function '%s' in module '%s'", ppu_get_function_name(module_name, fnid), module_name);
 			}
+			*/
 			//else
 			{
 				// Static function
